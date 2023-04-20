0702bd227873d5bf32cade6c2714c33b1811b70b
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-04-20 15:17:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+ char lista_nomi[3][5] = {"", "", ""};
+ char nome[5] = {""};
+ 
+ cin >> lista_nomi[0];
+ cin >> lista_nomi[1];
+ cin >> lista_nomi[2];
+ cout << endl;
+ cin >> nome;
+   
+   if((nome[0]==lista_nomi[0][0])and(nome[1]==lista_nomi[0][1])and(nome[2]==lista_nomi[0][2])and(nome[3]=lista_nomi[0][3])and(nome[4]==lista_nomi[0][4])){
+      cout<<"0";
+   }
+   else if((nome[0]==lista_nomi[1][0])and(nome[1]==lista_nomi[1][1])and(nome[2]==lista_nomi[1][2])and(nome[3]==lista_nomi[1][3])and(nome[4]==lista_nomi[1][4])){
+      cout<<"1";
+   }
+   else if((nome[0]==lista_nomi[2][0])and(nome[1]==lista_nomi[2][1])and(nome[2]==lista_nomi[2][2])and(nome[3]==lista_nomi[2][3])and(nome[4]==lista_nomi[2][4])) {
+      cout<<"2";
+   else {
+      cout<<"";  
+   }   
+
    return 0;
 }
